e19518c3306a0ae7f3fca180376c14672d7dd3bd
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-17 14:32:53 -0700

    Connecting Mongoose to MOngoDB

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express =require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+const key = require('./config/keys');
 require('./services/passport');
 
 const app = express();
 
+mongoose.connect(key.mongoURI, { useNewUrlParser: true })
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
