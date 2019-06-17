b564356dff21dbd083f868aefe38efca6ee982b0
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-17 15:39:35 -0700

    Enabling Cookies

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport'); 
 const key = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [key.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(key.mongoURI, { useNewUrlParser: true })
 require('./routes/authRoutes')(app);
