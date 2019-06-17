01dd39c7bc027236284dea07ddba70a658aaa579
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-17 10:45:31 -0700

    Add Google Strategy

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express =require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
 // new GoogleStrategy contains a new instance of the new google strategy.
 // Inside GoogleStrategy() we will show how what params to use the Google strategy
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.gogoleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken)=> {
+    console.log(accessToken);
+}));
 
 app.get("/", (req, res) => {
     res.send({hello: 'there'})
