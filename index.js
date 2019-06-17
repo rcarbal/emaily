75925aad9618ba27c71efa86c0070a2d9174755d
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-17 10:20:56 -0700

    Passport Setup

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express =require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+// new GoogleStrategy contains a new instance of the new google strategy.
+// Inside GoogleStrategy() we will show how what params to use the Google strategy
+passport.use(new GoogleStrategy());
 
 app.get("/", (req, res) => {
     res.send({hello: 'there'})
