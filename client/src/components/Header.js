47786f265ddd24caf8f146955dfa2a882f07b825
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-19 23:48:42 -0700

    Redirect on Logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,7 +12,7 @@ class Header extends React.Component {
                     </li>);
 
             default:
-                return <li><a>Logout</a></li>;
+                return <li><a href="/api/logout">Logout</a></li>;
         }
     }
 
