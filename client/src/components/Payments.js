1cddfecffa5a8e2696899c01d031e115e35f8066
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-20 12:30:55 -0700

    Payment Fixes

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
-class Payments extends React.Component{
-    render(){
+class Payments extends React.Component {
+    render() {
         return (
-            <StripeCheckout 
+            <StripeCheckout
+                name="Emaily"
+                description="$5 for 5 email credits"
                 amount={500}
-                token={ token => console.log(token)}
+                token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn">
+                    Add Credits
+                    </button>
+            </StripeCheckout>
         );
     }
 }
