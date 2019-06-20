e0ab1e8dcccff414f217da8642dc287475bf88de
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-19 23:40:46 -0700

    Redirecting a user on Auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,12 @@ app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google'),
+    (req, res)=>{
+        res.redirect('surveys');
+    })
 
 app.get('/api/logout', (req, res) =>{
     req.logout();
