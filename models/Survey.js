194b9638fb849ae854b67bfb4c619321fcc722e0
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-25 08:33:36 -0700

    Model deficiencies

--- models/Survey.js
+++ models/Survey.js
@@ -5,7 +5,9 @@ const surverySchema = new Schema({
     title: String, 
     body: String,
     subject: String,
-    recipients: [String]
+    recipients: [String],
+    yes: {type: Number, defalut: 0},
+    no: {type: Number, defalut: 0}
 });
 
 mongoose.model('surverys', surverySchema);
