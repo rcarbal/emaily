90976f2b6cd1d188e6cd1d81fcf8b8a0e767ab46
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-25 11:39:55 -0700

    Survey Creation Route Handler

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport'); 
 const bodyParser = require('body-parser');
 const key = require('./config/keys');
+require('./models/Survey');
 require('./models/User');
 require('./services/passport');
 
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
     // Express will serve up production assets
--- models/Survey.js
+++ models/Survey.js
@@ -8,7 +8,10 @@ const surverySchema = new Schema({
     subject: String,
     recipients: [RecipientSchema],
     yes: {type: Number, defalut: 0},
-    no: {type: Number, defalut: 0}
+    no: {type: Number, defalut: 0},
+    _user: { type: Schema.Types.ObjectId, ref: 'User' },
+    dateSent: Date,
+    lastResponded: Date
 });
 
 mongoose.model('surverys', surverySchema);
