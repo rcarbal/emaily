240f13d097b1609e084dd5a78c118c77090afaa6
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-25 15:56:39 -0700

    Boilerplate for sending Emmails

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,7 +10,26 @@ class Mailer extends helper.Mail {
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
+
+        // Register the body with the Mailer
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
     }
+
+    formatAddresses(recipients){
+        return recipients.map(({email})=>{
+            return new helper.Email(email);
+        });
+    };
+
+    addClickTracking(){
+        const trackingSettings = new helper.trackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    };
 
 }
 
