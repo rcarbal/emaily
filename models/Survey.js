a0535b1baca6996beddd994331f4366691792e1a
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-25 11:15:05 -0700

    Srtting up subDocs

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surverySchema = new Schema({
     title: String, 
     body: String,
     subject: String,
-    recipients: [String],
+    recipients: [RecipientSchema],
     yes: {type: Number, defalut: 0},
     no: {type: Number, defalut: 0}
 });
