2c3ddee2d1b2b27cdef4555c4fc3b66d20a80cdf
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-25 13:50:16 -0700

    Creating Subdoc Collections:

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -5,7 +5,16 @@ const requireCredits = require('../middlewares/requireCredit')
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res)=>{
+    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
         const { title, subject, body, recipients } = req.body;
-    })
-}
+
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({ email: email })),
+            _user: req.user.id,
+            dateSent: Date.now()
+        });
+    });
+};
