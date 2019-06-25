12434f6781821c5df06fd78775e2cf34743b7140
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-25 15:46:01 -0700

    Mailer Constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,7 +2,15 @@ const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail{
+class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
+
+        this.from_email = new helper.Email('ricardo.a.carballo@gmail.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+    }
 
 }
 
