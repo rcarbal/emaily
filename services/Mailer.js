7ac1d101858d26a528f35e4ea8317fc06cf88a5f
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-25 17:09:01 -0700

    Testing email sending through console

--- services/Mailer.js
+++ services/Mailer.js
@@ -25,7 +25,7 @@ class Mailer extends helper.Mail {
     };
 
     addClickTracking(){
-        const trackingSettings = new helper.trackingSettings();
+        const trackingSettings = new helper.TrackingSettings();
         const clickTracking = new helper.ClickTracking(true, true);
 
         trackingSettings.setClickTracking(clickTracking);
