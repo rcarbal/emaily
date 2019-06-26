a11587b66a95e6030a9f03811da0ed2cf07af36f
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-26 13:38:50 -0700

    Styling the Form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,13 +2,14 @@
 import _ from 'lodash';
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 
 const FIELDS = [
-    {label: 'Survey Title', name: 'title'},
-    {label: 'Subject Line', name: 'subject'},
-    {label: 'Email Body', name: 'body'},
-    {label: 'Recipient List', name: 'emails'}
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' }
 ]
 
 class SurveyForm extends React.Component {
@@ -25,7 +26,13 @@ class SurveyForm extends React.Component {
                     onSubmit={this.props.handleSubmit(values => console.log(values))}
                 >
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Submit
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
